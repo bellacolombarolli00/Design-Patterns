513eb896a60efe1586df2c67c8f7986331e5b689
bellacolombarolli00 <101637594+bellacolombarolli00@users.noreply.github.com>
2022-11-09 09:36:00 +0000

    until reverse with string builder

--- src/main/java/org/example/StringInverter.java
+++ src/main/java/org/example/StringInverter.java
@@ -3,12 +3,7 @@ package org.example;
 public class StringInverter implements  StringTransformer{
     @Override
     public void execute(StringDrink drink) {
-        String text = drink.getText();
-        String newText = "";
-        for(int i = 0; i<text.length(); i++) {
-            char c = text.charAt(i);
-            newText = c+newText;
-        }
-        drink.setText(newText);
+        StringBuilder text = new StringBuilder(drink.getText());
+        drink.setText(text.reverse().toString());
     }
 }
